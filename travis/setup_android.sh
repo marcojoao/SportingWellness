15cb3a1a3998c05929d4282d28914f2acd41d7f0
Marco João <marcojoao11@gmail.com>
2019-07-24 21:23:37 +0100

    trying to fix sdkmanager

--- travis/setup_android.sh
+++ travis/setup_android.sh
@@ -26,6 +26,10 @@ echo "==> [INSTALLING ANDROID NDK]"
 brew cask install android-ndk
 export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 
+echo "==> [SDK-MANAGER LIST]"
+export SDKMANAGER_OPTS="--add-modules java.se.ee"
+sdkmanager --list
+
 echo "==> [SDK-MANAGER UPDATE]"
 yes y | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager --update
 ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "build-tools;${androidBuildToolsVersion}" "platform-tools" "platforms;${androidPlatformVerion}"
