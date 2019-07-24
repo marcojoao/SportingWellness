c153eb8cf065c373303d150fcee55793eae881c5
Marco Joao <marcojoao11@gmail.com>
2019-07-24 18:35:00 +0100

    fix path name

--- travis/setup_android.sh
+++ travis/setup_android.sh
@@ -1,29 +1,25 @@
 echo "::::::::::::::::::[SETUP ANDROID]::::::::::::::::::"
-
 androidBuildToolsVersion="28.0.3"
 androidPlatformVerion="android-28"
 
-echo "@ ===> Brew Doctor"
+echo "==> [BREW DOCTOR]"
 brew doctor
 
-echo "@ ===> Brew update"
+echo "==> [BREW UPDATE]"
 brew update
 
-echo "@ ===> Installing Java 8"
+echo "==> [INSTALLING JAVA 8]"
 brew cask install caskroom/versions/java8
 export JAVA_HOME=$(/usr/libexec/java_home)
 
-echo "@ ===> Installing Android SDK"
+echo "==> [INSTALLING ANDROID SDK]"
 brew cask install android-sdk
-#export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
-export ANDROID_HOME="/usr/local/opt/android-sdk"
+export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
 
-
-echo "@ ===> Installing Android NDK"
+echo "==> [INSTALLING ANDROID NDK]"
 brew cask install android-ndk
-#export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
-export ANDROID_NDK_HOME="/usr/local/opt/android-ndk"
+export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 
-echo "===> Executing SDK Manager"
-yes y | ${ANDROID_HOME}/tools/bin/sdkmanager --update
-${ANDROID_HOME}/tools/bin/sdkmanager "build-tools;${androidBuildToolsVersion}" "platform-tools" "platforms;${androidPlatformVerion}"
+echo "==> [SDK-MANAGER UPDATE]"
+yes y | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager --update
+${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "build-tools;${androidBuildToolsVersion}" "platform-tools" "platforms;${androidPlatformVerion}"
