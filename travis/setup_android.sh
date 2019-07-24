5fcdd3483e16cd9ffbd598f7c4ba332fbf0621fa
Marco Joao <marcojoao11@gmail.com>
2019-07-24 17:46:42 +0100

    trying again

--- travis/setup_android.sh
+++ travis/setup_android.sh
@@ -6,23 +6,19 @@ androidPlatformVerion="android-28"
 brew update
 
 echo "@ ===> Setup Java 8"
-brew cask uninstall java
-brew tap caskroom/versions
-brew cask install java8
+brew cask install caskroom/versions/java8
 touch ~/.android/repositories.cfg
+set JAVA_OPTS=-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee
 export JAVA_HOME=$(/usr/libexec/java_home)
 
-echo "===> Executing: brew cask install android-sdk"
+echo "@ ===> Setup Android SDK"
 brew cask install android-sdk
-echo "===> Executing:: export ANDROID_SDK_ROOT=\"/usr/local/share/android-sdk\""
 export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
 
-echo "===> Executing: brew cask install android-ndk"
+echo "@ ===> Setup Android NDK"
 brew cask install android-ndk
-echo "===> Executing: export ANDROID_NDK_HOME=\"/usr/local/share/android-ndk\""
 export ANDROID_NDK_HOME="/usr/local/share/android-ndk"
 
-echo "===> Executing: sdkmanager --update"
+echo "===> Execute SDK Manager"
 yes y | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager --update
-echo "===> Executing: sdkmanager \"build-tools;${androidBuildToolsVersion}\" \"platform-tools\" \"platforms;${androidPlatformVerion}\""
 ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "build-tools;${androidBuildToolsVersion}" "platform-tools" "platforms;${androidPlatformVerion}"
