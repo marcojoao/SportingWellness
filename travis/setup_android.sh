15ac441cd95f6aa72082a990729fd0e62517de73
Marco Joao <marcojoao11@gmail.com>
2019-07-24 18:46:51 +0100

    trying again

--- travis/setup_android.sh
+++ travis/setup_android.sh
@@ -9,12 +9,13 @@ echo "==> [BREW UPDATE]"
 brew update
 
 echo "==> [INSTALLING JAVA 8]"
-brew cask install caskroom/versions/java8
-export JAVA_HOME=$(/usr/libexec/java_home)
+brew cask install homebrew/cask-versions/java8
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
 echo "==> [INSTALLING ANDROID SDK]"
 brew cask install android-sdk
 export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+mkdir -p "$ANDROID_SDK_ROOT/licenses"
 
 echo "==> [INSTALLING ANDROID NDK]"
 brew cask install android-ndk
